e101b8fd61176ed7c67366dadc3203d20f32e0f3
mateu <vitor.m.rosa@aluno.senai.br>
2024-11-20 18:47:56 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/e1/Aluno.java
@@ -0,0 +1,29 @@
+package com.example.e1;
+
+public class Aluno {
+
+    String nome;
+    float nota;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    // somar todas as notas e dividir com o tamanho da lista
+    //fazer um for da lista de usuario e criar um variavel somadora
+
+
+}
+
